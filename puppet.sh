5ce4ea334c450618a1eb0229d602e600774f66b8
Graham Gilbert <graham@grahamgilbert.com>
2016-04-01 12:51:42 +0100

    Try CD-ing

--- puppet.sh
+++ puppet.sh
@@ -4,9 +4,9 @@
 gem install r10k
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+cd $DIR
 # Run r10k with Puppetfile
-r10k puppetfile install "${DIR}/Puppetfile" -v
+r10k puppetfile install "Puppetfile" -v
 
 # Run Puppet Apply with grahamconfig class
 /opt/puppetlabs/bin/puppet apply -e "include grahamconfig"
