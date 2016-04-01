fceca00283e9496ac0f886e79e03657c2844af0e
Graham Gilbert <graham@grahamgilbert.com>
2016-04-01 11:26:09 +0100

    Follow redirects

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ AUTOPKG_DIR=$(mktemp -d /tmp/autopkg-XXXX)
 
 AUTOPKG_PKG=${AUTOPKG_DIR}/AutoPkg.pkg
 mkdir -p ${AUTOPKG_DIR}
-curl -o ${AUTOPKG_PKG} https://github.com/autopkg/autopkg/releases/download/v0.6.1/autopkg-0.6.1.pkg
+curl -o ${AUTOPKG_PKG} -L https://github.com/autopkg/autopkg/releases/download/v0.6.1/autopkg-0.6.1.pkg
 installer -pkg "${AUTOPKG_PKG}" -tgt /
 
 # AUTOPKG="/usr/local/bin/autopkg"
