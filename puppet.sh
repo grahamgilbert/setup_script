83b4a4599317eac533bb31926ad8e5c3d537c5dc
Graham Gilbert <graham.gilbert@airbnb.com>
2016-11-28 16:13:54 -0800

    Mailplane

--- puppet.sh
+++ puppet.sh
@@ -4,9 +4,9 @@
 gem install r10k
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd "$DIR"
 # Run r10k with Puppetfile
 r10k puppetfile install "Puppetfile" -v
 
 # Run Puppet Apply with grahamconfig class
-/opt/puppetlabs/bin/puppet apply -e "include grahamconfig"
+/opt/puppetlabs/bin/puppet apply -e "class {'grahamconfig': my_username => 'graham_gilbert', my_homedir => '/Users/graham_gilbert', my_sourcedir => '/Users/graham_gilbert/src' }"
