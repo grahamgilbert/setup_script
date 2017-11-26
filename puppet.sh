9c414cf068e3502cb0fea4af5f51be2d4cbc527d
Graham Gilbert <graham@grahamgilbert.com>
2017-11-26 11:56:06 -0800

    Update puppet.sh

--- puppet.sh
+++ puppet.sh
@@ -8,5 +8,7 @@ cd "$DIR"
 # Run r10k with Puppetfile
 r10k puppetfile install "Puppetfile" -v
 
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
 # Run Puppet Apply with grahamconfig class
-/opt/puppetlabs/bin/puppet apply -e "class {'grahamconfig': my_username => 'graham_gilbert', my_homedir => '/Users/graham_gilbert', my_sourcedir => '/Users/graham_gilbert/src' }"
+/opt/puppetlabs/bin/puppet apply -e "class {'grahamconfig': my_username => $loggedInUser', my_homedir => '/Users/$loggedInUser', my_sourcedir => '/Users/$loggedInUser/src' }"
